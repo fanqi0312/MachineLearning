a677cfde720dc802c1cb62df0560747f2f445016
fanqi0312 <fanqi0312@sina.com>
2017-10-10 16:45:04 +0800

    神经网络

--- MachineLearningBase/NeuralNetwork/NeuralNetwork.py
+++ MachineLearningBase/NeuralNetwork/NeuralNetwork.py
@@ -1,30 +1,34 @@
+# 数值计算库，存储和处理大型矩阵
 import numpy as np
 
-
+# tan函数
 def tanh(x):
     return np.tanh(x)
 
-
+# tan函数倒数
 def tanh_deriv(x):
     return 1.0 - np.tanh(x) * np.tanh(x)
 
-
+# 逻辑函数
 def logistic(x):
     return 1 / (1 + np.exp(-x))
 
-
+# 逻辑函数倒数
 def logistic_derivative(x):
     return logistic(x) * (1 - logistic(x))
 
-
+# 神经网络类
 class NeuralNetwork:
+    # 构造函数（层数个数，模式）
     def __init__(self, layers, activation='tanh'):
         """  
         :param layers: A list containing the number of units in each layer.
-        Should be at least two values  
+        Should be at least two values
+
         :param activation: The activation function to be used. Can be
-        "logistic" or "tanh"  
+        "logistic" or "tanh"  （默认）
         """
+        # 初始化-使用双曲函数或逻辑函数计算
         if activation == 'logistic':
             self.activation = logistic
             self.activation_deriv = logistic_derivative
@@ -32,28 +36,44 @@ class NeuralNetwork:
             self.activation = tanh
             self.activation_deriv = tanh_deriv
 
+        # 初始化-随机设置权重
         self.weights = []
         for i in range(1, len(layers) - 1):
             self.weights.append((2 * np.random.random((layers[i - 1] + 1, layers[i] + 1)) - 1) * 0.25)
             self.weights.append((2 * np.random.random((layers[i] + 1, layers[i + 1])) - 1) * 0.25)
-
+    """ 
+    X   训练集
+    y   分类标记
+    learning_rate   学习率
+    epochs  抽样计算10000次
+    """
     def fit(self, X, y, learning_rate=0.2, epochs=10000):
+        ############# 处理X
+        # 确认是二维
         X = np.atleast_2d(X)
+        # 1矩阵（行数，列数+1）
         temp = np.ones([X.shape[0], X.shape[1] + 1])
-        temp[:, 0:-1] = X  # adding the bias unit to the input layer         
+        # 第一列和除最后一列
+        temp[:, 0:-1] = X  # adding the bias unit to the input layer
         X = temp
+        # 转化标准array
         y = np.array(y)
 
+        ############# 更新神经网络（核心）
         for k in range(epochs):
+            # 从0-10随机取数
             i = np.random.randint(X.shape[0])
+            # 随机实例
             a = [X[i]]
 
             for l in range(len(self.weights)):  # going forward network, for each layer
-                a.append(self.activation(np.dot(a[l], self.weights[
-                    l])))  # Computer the node value for each layer (O_i) using activation function
+                # Computer the node value for each layer (O_i) using activation function
+                a.append(self.activation(np.dot(a[l], self.weights[l])))
+            # 误差值
             error = y[i] - a[-1]  # Computer the error at the top layer
-            deltas = [
-                error * self.activation_deriv(a[-1])]  # For output layer, Err calculation (delta is updated error)
+            # 反向更新
+            # For output layer, Err calculation (delta is updated error)
+            deltas = [error * self.activation_deriv(a[-1])]
 
             # Staring backprobagation
             for l in range(len(a) - 2, 0, -1):  # we need to begin at the second to last layer
@@ -65,6 +85,7 @@ class NeuralNetwork:
                 delta = np.atleast_2d(deltas[i])
                 self.weights[i] += learning_rate * layer.T.dot(delta)
 
+    # 测试（与正向相似）
     def predict(self, x):
         x = np.array(x)
         temp = np.ones(x.shape[0] + 1)
--- MachineLearningBase/NeuralNetwork/NeuralNetworkTest.py
+++ MachineLearningBase/NeuralNetwork/NeuralNetworkTest.py
@@ -1,9 +1,13 @@
 from NeuralNetwork import NeuralNetwork
 import numpy as np
 
+# 实例化神经网络，2,2,1神经网络
 nn = NeuralNetwork([2, 2, 1], 'tanh')
+
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y = np.array([0, 1, 1, 0])
+
 nn.fit(X, y)
+
 for i in [[0, 0], [0, 1], [1, 0], [1, 1]]:
     print(i, nn.predict(i))
