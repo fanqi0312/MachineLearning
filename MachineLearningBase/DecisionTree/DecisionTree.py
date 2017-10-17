888903cc0f9db5a3ffcaa3c31b41dae16537dbed
fanqi0312 <fanqi0312@sina.com>
2017-10-17 17:39:59 +0800

    1

--- MachineLearningBase/DecisionTree/DecisionTree.py
+++ MachineLearningBase/DecisionTree/DecisionTree.py
@@ -9,6 +9,8 @@ from sklearn.feature_extraction import DictVectorizer
 import csv
 from sklearn import preprocessing
 from sklearn import tree
+from IPython.display import Image
+import pydotplus
 from sklearn.externals.six import StringIO
 
 
@@ -39,11 +41,13 @@ allElectronicsData.close()
 # 2.转化数据为sklearn要求的数据
 # 转化featureList
 vec = DictVectorizer()
+#转化为01矩阵，每个属性一列共10维，用0.1记录
+# 共提取10个参数，['age=middle_aged', 'age=senior ', 'age=youth ', 'credit_rating=excellent ', 'credit_rating=fair ', 'income=high ', 'income=low ', 'income=medium ', 'student=no ', 'student=yes ']
 dummyX = vec.fit_transform(featureList).toarray()
 print("dummyX:" + str(dummyX))
 print("vec.get_feature_names():" + str(vec.get_feature_names()))
 
-# 转化labelList
+# 转化labelList，0位no，1位yes
 print("labelList:" + str(labelList))
 lb = preprocessing.LabelBinarizer()
 dummyY = lb.fit_transform(labelList)
@@ -62,16 +66,21 @@ with open("DecisionTree.dot", 'w') as f:
     f = tree.export_graphviz(clf, feature_names=vec.get_feature_names(), out_file=f)
 
 
-## 5.给定新的数据进行预测
+## 5.可是化决策树
+# 1. 打开CMD
+# 2. cd dot所在目录
+# 3. dot –Tpdf DecisionTree.dot –o DecisionTree.pdf
+
+## 6.给定新的数据进行预测
 # 创建测试数据
 oneRowX = dummyX[0, :]
-print("oneRowX:" + str(oneRowX))
+print("读取第一条oneRowX:" + str(oneRowX))
 newRowX = oneRowX
 # 修改数据
 newRowX[0] = 1
 newRowX[2] = 0
-print("newRowX:" + str(newRowX))
+print("修改为新的测试数据newRowX:" + str(newRowX))
 # 使用决策树测试
 predictedY = clf.predict(newRowX)
-print("predictedY:" + str(predictedY))
-#购买1
+print("预测结果predictedY:" + str(predictedY))
+#结果：购买1
