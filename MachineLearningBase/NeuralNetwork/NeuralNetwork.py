85e503d7366c266108935adf1c502c5e77c2916d
fanqi0312 <fanqi0312@sina.com>
2018-03-04 17:51:36 +0800

    删除练习代码

--- MachineLearningBase/NeuralNetwork/NeuralNetwork.py
+++ MachineLearningBase/NeuralNetwork/NeuralNetwork.py
@@ -114,3 +114,21 @@ def logistic(x):
 # 求logistic导数
 def logistic_derivative(x):
     return logistic(x) * (1 - logistic(x))
+
+"""
+运行测试
+"""
+# 实例化神经网络，2,2,1神经网络
+nn = NeuralNetwork([2, 2, 1], 'tanh')
+
+# 训练集
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+Y = np.array([0, 1, 1, 0])
+
+# 训练
+nn.fit(X, Y, 0.3, 30000)
+
+# 测试集
+# for i in [[0, 0], [0, 1], [1, 0], [1, 1]]:
+for i in X:
+    print(i, nn.predict(i))
